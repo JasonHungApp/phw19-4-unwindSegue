b87b7a0387fad05be582254c33bf9890c8313732
jasonhung <hello@ifhavebugcallme.com>
2024-01-09 19:22:59 +0800

    ver 2024.01.09

---
+++ phw19-4-unwindSegue/CitiesTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BTableViewController.swift
+//  phw19-4-unwindSegue
+//
+//  Created by jasonhung on 2024/1/9.
+//
+
+import UIKit
+
+class citiesTableViewController: UITableViewController {
+
+    // 完整的台灣縣市列表
+        let cities = ["台北市", "新北市", "桃園市", "台中市", "台南市", "高雄市", "基隆市", "新竹市", "新竹縣", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "屏東縣", "宜蘭縣", "花蓮縣", "台東縣", "澎湖縣", "金門縣", "連江縣"]
+
+        var selectedValue: String?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+
+        // MARK: - Table view data source
+
+        override func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return cities.count
+        }
+
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell", for: indexPath)
+            cell.textLabel?.text = cities[indexPath.row]
+            return cell
+        }
+
+        // MARK: - Table view delegate
+
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            selectedValue = cities[indexPath.row]
+           // performSegue(withIdentifier: "unwindToFirstViewController", sender: self)
+        }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       if let cell = sender as? UITableViewCell,
+          let indexPath = tableView.indexPath(for: cell) {
+           selectedValue = cities[indexPath.row]
+       }
+    }
+
+}
